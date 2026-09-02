9ef330d31c308fe1fe631f106b52a0e8d373853a
Jack Burky <76707499+jsburky@users.noreply.github.com>
2026-09-02 15:51:38 -0400

    Linked to firebase

--- syncinary/lib/main.dart
+++ syncinary/lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'pages/itinerary_builder.dart';
 import 'theme/app_theme.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
